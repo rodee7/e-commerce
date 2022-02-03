93eeb65d71c238a8d09b7b76565db865df8930f7
“rodee7” <deepugovindan@gmail.com>
2022-02-03 12:47:50 +0100

    updated app.js

--- app.js
+++ app.js
@@ -17,12 +17,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 // app.engine('hbs', hbs.engine({extName:'hbs', defaultLayout:'layout', layoutsDir:__dirname+'/views/layout/', partialsDir:__dirname+'/views/partials/'}))
-app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layout/', partialsDir: __dirname + '/views/partials/', runtimeOptions: { allowProtoPropertiesByDefault: true, allowProtoMethodsByDefault: true,},}))
+app.engine('hbs', hbs.engine({ extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + 
+'/views/layout/', partialsDir: __dirname + '/views/partials/', 
+runtimeOptions: { allowProtoPropertiesByDefault: true, allowProtoMethodsByDefault: true,},}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(session({secret: "Key", cookie: {maxAge: 1800000}, resave: true, saveUninitialized: true}))
 
 db.connect((err) =>{
