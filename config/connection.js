32398de6932552928602acba6e2521c054b0a182
“rodee7” <deepugovindan@gmail.com>
2022-02-03 21:34:53 +0100

    updated db url

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 
 module.exports.connect = (done) => {
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb+srv://rodee7:Prograd%402021@cluster0.vrs9f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     const dbName = 'shopping'
     // mongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, (err, data) => {
     mongoClient.connect(url,(err, data) => {
